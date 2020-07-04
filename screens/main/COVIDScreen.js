9054cd84a9aa22fd3c307add529d307292ea9cb5
Sophia Patterson <Sophia@Sophias-Air.sjc.vegetablesandfruits.org>
2020-07-03 17:39:53 -0700

    modification to settings and enabled Apple maps from home screen maps button

--- screens/main/COVIDScreen.js
+++ screens/main/COVIDScreen.js
@@ -4,9 +4,11 @@
  */
 import React from "react"
 import {View, Text} from "react-native"
+import ArmyLogo from "../../components/ArmyLogo"
 export default function COVIDScreen() {
     return (
         <View>
+            <ArmyLogo/>
             <Text>Screen under construction.</Text>
             <Text>COVID</Text>
         </View>
