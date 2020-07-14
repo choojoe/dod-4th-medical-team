717acdd526e9e26822415a6f47f4a31d361929b2
Daniel Hwang <dhwang314@gmail.com>
2020-07-14 09:33:52 -0400

    Update SettingsScreen.js
    get rid of sensitive info

--- screens/sidebar/SettingsScreen.js
+++ screens/sidebar/SettingsScreen.js
@@ -3,38 +3,12 @@
  * ROUTE NAME: Settings
  */
 import React from "react"
-import { Button } from "react-native"
-import {View, Text, Linking} from "react-native"
-import {ProviderTypes, TranslatorConfiguration, TranslatorFactory } from 'react-native-power-translator';
-function test() {
-    const translator = TranslatorFactory.createTranslator();
-    translator.translate('Engineering physics or engineering science').then(translated => {
-        console.log(translated)
-    })
-}
-
+import {View, Text} from "react-native"
 export default function SettingsScreen() {
-    const [language, setLanguage] = React.useState("en")
     return (
-        <View>
-            <Button
-                title = "Settings"
-                onPress={() => {
-                    Linking.openSettings();
-            }}
-            />
-            <Button
-                title = { "Translate to " + (language === "en" ? "Spanish" : "English") }
-                onPress={() => {
-                    TranslatorConfiguration.setConfig(ProviderTypes.Google, 'AIzaSyB8Wozd7rLfU-EMOfI98rg9XenVzwZkhbA', language === "es" ? "en" : "es" , language);
-                    setLanguage( language === "es" ? "en" : "es" )
-            }}
-            />
-            <Button 
-                title = "Test function"
-                onPress = {() => {test()}}
-            />
-
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>This is a test screen.</Text>
+            <Text>Settings</Text>
         </View>
-    );
+    )
 }
