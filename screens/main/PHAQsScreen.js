04e32887ebedb7cd326d0daf546ed7e40646e4d8
Daniel Hwang <dhwang314@gmail.com>
2020-08-04 18:18:42 -0400

    PHAQsScreen, finalization
    I gave up on the download screen since it requires use of native module react-native-fs. Provided some rough code.

--- screens/main/PHAQsScreen.js
+++ screens/main/PHAQsScreen.js
@@ -9,38 +9,56 @@ import {View, Button, StyleSheet, Text} from "react-native"
 //allow us to configure i18n.
 import {useTranslation} from "react-i18next"
 
-//allows us to extract URI from local file
-import {Asset} from "expo-asset";
-import * as FileSystem from 'expo-file-system';
+// Packages required to download the PDF
+import {Asset} from "expo-asset"
+
+// Packages required to upload the PDF
 import * as MailComposer from "expo-mail-composer";
 import * as DocumentPicker from "expo-document-picker"
 
 
 export default function PHAQsScreen({navigation}) {
+    //We set two variables: status (what the user sees upon each output) and file (details about the file that users uploaded)
+    //setStatus and setFile are the functions used to change our variables' values, respectively.
     const [status, setStatus] = useState("")
     const [file, setFile] = useState(undefined)
 
+    //enables us to use local translations
     const {t} = useTranslation();
 
+    //WARNING: THIS CODE REQUIRES USE OF NATIVE PACKAGE REACT-NATIVE-FS. IT CANNOT
+    //BE TESTED ON EXPO SINCE IT IS A NATIVE PACKAGE. Rough code is provided, but
+    // we cannot guarantee that it will work properly.
+    //https://www.npmjs.com/package/react-native-fs
     const downloadPDF = async () => {
-        
+        const RNFS = require("react-native-fs") //store the React Native FS variable here.
+        const PHAQsURI = Asset.fromModule(require('../../assets/PHAQ.pdf')).uri; //use the Asset library to get the uri of our PHAQ file stored on our directory
+        RNFS.downloadFile({ //download the file
+            fromUrl: PHAQsURI, //download from our PHAQs file above
+            toFile: `${RNFS.DocumentDirectoryPath}/PHAQ.pdf`, //save it onto our local path
+          }).then((r) => {
+            setStatus("File downloaded! Check your documents folder!") //upon success, display success message!
+        });
     }
 
     const uploadPDF = async () => {
+        //wait for the user to choose a file. Must be a pdf.
+        //https://docs.expo.io/versions/latest/sdk/document-picker/
         const pdf = await DocumentPicker.getDocumentAsync({
-            type: "application/pdf",
-            copyToCacheDirectory: true,
+            type: "application/pdf", //specify type pdf
+            copyToCacheDirectory: true, //we save a local version of the file in the cache.
         })
         if (!pdf){
             setStatus("You didn't submit a file!")
             return ;
         }
-        console.log(pdf)
-        setStatus("Uploaded "+ pdf.name)
-        setFile(pdf)
+        setStatus("Uploaded "+ pdf.name) //if a pdf was submitted, then display a success message containing the name of the file
+        setFile(pdf) //save the file details using setFile.
     }
 
     const sendEmail = async () => {
+        //send an email using the file stored from uploadPDF
+        // see https://docs.expo.io/versions/latest/sdk/mail-composer/
         MailComposer.composeAsync({
             recipients: ["Johnmarth.D.Perez.mil@mail.mil"],
             subject: "PHAQ Submission",
@@ -48,18 +66,22 @@ export default function PHAQsScreen({navigation}) {
             attachments: [file.uri]
         })
     }
+    //Return a list of three buttons: to download PHAQs, to upload PHAQs, and to
+    //send PHAQs by email. Note that the send feature is disabled if there is no active file.
+    // Download PHAQs is disabled right now due to lack of testing.
     return (
         <View style = {styles.container}>
             <Button 
-                title = {"Download PHAQs"}
-                onPress = {() => downloadPDF()}
+                title = {t("Download PHAQs")}
+                disabled = {true}
+                onPress = {() => {downloadPDF()}}
             />
             <Button 
-                title = {"Upload PHAQs"}
+                title = {t("Upload PHAQs")}
                 onPress = {() => {uploadPDF()}}
             />
             <Button 
-                title = {"Send PHAQs"}
+                title = {t("Send PHAQs")}
                 disabled = {!file}
                 onPress = {() => {sendEmail()}}
             />
